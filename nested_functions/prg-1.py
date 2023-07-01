1351dc1cedaf89b4d0c19f1cc5176459c0f91c88
nanda kishore <nandakishore2422y@gmail.com>
2023-07-01 20:03:50 +0530

    python programs

---
+++ nested_functions/prg-1.py
@@ -0,0 +1,11 @@
+#nested functions
+def f1():
+    print("i am in f1")
+    def f2():
+        print("I am in F2")
+        def f3():
+            print("I am in F3")
+        f3()
+    f2()
+
+f1()
