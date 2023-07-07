6773dedf544f95997d502f2f67d59f836854cdaf
nanda kishore <nandakishore2422y@gmail.com>
2023-07-07 21:31:18 +0530

    python programs

---
+++ exception_handling/fun-1.py
@@ -0,0 +1,9 @@
+#excetion handling
+from math import sqrt
+a=10
+try:
+    b=sqrt(a)
+    print("sqrt is",b)
+except:
+    print("a value is",a)
+print("Welcome to python world")
---
+++ exception_handling/fun-3.py
@@ -0,0 +1,7 @@
+#program for divison using exception handling 
+try:
+ a = int(input("Enter Num1:"))
+ b = int(input("Enter Num2:"))
+ print("result is:", a / b)
+except Exception as msg:
+ print(msg)
