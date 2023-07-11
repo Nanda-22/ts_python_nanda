9f5e55dd7398f21052d1b422690539671bd500c1
nanda kishore <nandakishore2422y@gmail.com>
2023-07-11 22:03:19 +0530

    python programs

---
+++ variable_and_method_types/declare_instance_variable.py
@@ -0,0 +1,10 @@
+#Example program to declare instance variables
+class Test:
+    def __init__(self):
+        self.a=10
+    def m1(self):
+        self.b=20
+t=Test()
+t.m1()
+t.c=30
+print(t.__dict__)
